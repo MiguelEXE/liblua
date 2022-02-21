37e469e951b2f19c83d37b8dc87a092f520fc69b
Ocawesome101 <ocawesome101@gmail.com>
2022-02-21 14:42:23 -0500

    don't recall what i did

--- src/i18n.lua
+++ src/i18n.lua
@@ -1,10 +1,11 @@
 -- localization! --
 
 local sys = require("syscalls")
+local stdio = require("stdio")
 
 local lib = {}
 
-local maps = {}
+local mapcache = {}
 
 -- find a locale e.g. `/etc/lang/en_US-errors.lua`
 local function findmap(mapid, lang)
@@ -21,13 +22,46 @@ end
 
 local function loadmap(id)
   local lang = sys.getenv("LC_LOCALE") or "en_US"
+  local fullid = lang .. "-" .. id
+  if mapcache[fullid] then return mapcache[fullid] end
+
   local path, err = findmap(id, lang)
   if not path then
-    sys.printf(sys.stderr, err)
+    stdio.fprintf(stdio.stderr, "%s\n", err)
+    return nil, err
   end
+
+  local fd
+  fd, err = sys.open(path, "r")
+  if not fd then
+    stdio.fprintf(stdio.stderr, "%s: errno %d\n", err)
+    return nil, err
+  end
+
+  local data = sys.read(fd, "a")
+  sys.close(fd)
+
+  local call
+  call, err = load("return " .. data, "="..path, "t", {})
+  if not call then
+    stdio.fprintf(stdio.stderr, "%s\n", err)
+    return nil, err
+  end
+
+  local suc, ret = pcall(call)
+  if not suc then
+    stdio.fprintf(stdio.stderr, "%s\n", ret)
+    return nil, ret
+  end
+
+  mapcache[fullid] = ret
+
+  return ret
 end
 
 function lib.fetch(map, key)
+  local lmap = loadmap(map)
+  return lmap[key]
 end
 
 return lib
