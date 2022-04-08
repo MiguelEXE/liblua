000c3fcbf8103fc354fa628b977c130c16b045db
Ocawesome101 <ocawesome101@gmail.com>
2022-04-08 11:10:54 -0400

    add an option to getopt to allow repeated occurrences of the same option

--- src/getopt.lua
+++ src/getopt.lua
@@ -2,88 +2,129 @@
 
 local lib = {}
 
-function lib.getopt(_opts, ...)
+function lib.getopt(_opts, _args)
   checkArg(1, _opts, "table")
-  local _args = table.pack(...)
+  checkArg(2, _args, "table")
+
   local args, opts = {}, {}
   local skip_next, done = false, false
+
   for i, arg in ipairs(_args) do
     if skip_next then skip_next = false
     elseif arg:sub(1,1) == "-" and not done then
       if arg == "--" and opts.allow_finish then
         done = true
+
       elseif arg:match("%-%-(.+)") then
         arg = arg:sub(3)
+
         if _opts.options[arg] ~= nil then
           if _opts.options[arg] then
             if (not _args[i+1]) then
               io.stderr:write("option '", arg, "' requires an argument\n")
+
               if opts.help_message then
                 io.stderr:write(opts.help_message, "\n")
               end
+
               os.exit(1)
             end
-            opts[arg] = _args[i+1]
+
+            opts[arg] = opts[arg] or {}
+            table.insert(opts[arg], _args[i+1])
             skip_next = true
+
           else
             opts[arg] = true
+
           end
+
         elseif _opts.exit_on_bad_opt then
           io.stderr:write("unrecognized option '", arg, "'\n")
+
           if opts.help_message then
             io.stderr:write(opts.help_message, "\n")
           end
+
           os.exit(1)
         end
+
       else
         arg = arg:sub(2)
+
         if _opts.options[arg:sub(1,1)] then
           local a = arg:sub(1,1)
+
           if #arg == 1 then
             if not _args[i+1] then
               io.stderr:write("option '", arg, "' requires an argument\n")
+
               if opts.help_message then
                 io.stderr:write(opts.help_message, "\n")
               end
+
               os.exit(1)
             end
-            opts[a] = _args[i+1]
+
+            opts[a] = opts[a] or {}
+            table.insert(opts[a], _args[i+1])
             skip_next = true
+
           else
             opts[a] = arg:sub(2)
           end
+
         else
           for c in arg:gmatch(".") do
             if _opts.options[c] == nil then
               if _opts.exit_on_bad_opt then
                 io.stderr:write("unreciognized option '", arg, "'\n")
+
                 if opts.help_message then
                   io.stderr:write(opts.help_message, "\n")
                 end
+
                 os.exit(1)
               end
+
             elseif _opts.options[c] then
               if not _args[i+1] then
                 io.stderr:write("option '", arg, "' requires an argument\n")
+
                 if opts.help_message then
                   io.stderr:write(opts.help_message, "\n")
                 end
+
                 os.exit(1)
               end
+
               opts[c] = true
+
             else
               opts[c] = true
+
             end
           end
         end
       end
+
     else
       if _opts.finish_after_arg then
         done = true
       end
+
       args[#args+1] = arg
     end
   end
+
+  if not _opts.can_repeat_opts then
+    for k, v in pairs(opts) do
+      if type(v) == "table" then
+        opts[k] = v[#v]
+      end
+    end
+  end
+
   return args, opts
 end
 
